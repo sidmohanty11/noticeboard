b51fb773c4e146c5925774c8f48010a7ea902f9a
Sidharth Mohanty <sidmohanty11@gmail.com>
2022-04-19 19:59:59 +0530

    remove unused momentjs

--- server/index.js
+++ server/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cheerio = require("cheerio");
 const axios = require("axios");
-const moment = require("moment");
 
 const host = "https://www.cet.edu.in/";
 const url = "https://www.cet.edu.in/notice.php";
